c619ce3370c91eab9496ffb3511cb0baa221e77d
ubersk <91375104+Ubersk@users.noreply.github.com>
2023-02-05 08:16:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleLessons;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        User user1 = new User("","","","");
+        Console.WriteLine("Введите Имя:");
+        user1.Name = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Введите Фамилию:");
+        user1.LastName = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Введите Отчество:");
+        user1.MiddleName = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Расскажите о себе:");
+        user1.Description= Convert.ToString(Console.ReadLine());
+
+
+        user1.Print();
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLessons
+{
+    public class User
+    {
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? Description { get; set; }
+
+
+        public User(string name, string lastName, string middleName, string description)
+        {
+            Login=CreateLogin();
+            Password=CreatePassword();
+            Name=name;
+            LastName=lastName;
+            MiddleName=middleName;
+            Description=description;
+        }
+
+        public string CreatePassword()
+        {
+            string Pas = "12345";
+            return Pas;
+        }
+
+        public string CreateLogin()
+        {
+            string Log = "Example";
+            return Log;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Ваши данные. Всё верно?");
+            Console.WriteLine($"ФИО:{Name} {LastName} {MiddleName}");
+            Console.WriteLine("Сгенерирован логин...");
+            Console.WriteLine($"Логин:{Login}");
+            Console.WriteLine("Сгенерирован пароль...");
+            Console.WriteLine($"Пароль:{Password}");
+            Console.WriteLine($"О себе:{Description}");
+        }
+    }
+}
